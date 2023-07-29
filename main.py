334d5fe3857c58329777263f5a506228fb6c4cdd
Donya Navabi <nvbdnya@gmail.com>
2023-07-29 12:32:45 +0330

    complete

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import csv
-import codecs
+
 
 def handle_error(url):
     """Handles errors that may occur when scraping the website.
@@ -51,8 +51,8 @@ def scrape(url):
 
 
 def QA_datas(QA_URL_list):
-    #for url in QA_URL_list:
-        soup = scrape(QA_URL_list[0][0])
+    for url in QA_URL_list:
+        soup = scrape(url[0])
         topic = soup.find("title").text.replace(" | تبادل نظر نی نی سایت", '').strip()
         topic_data = soup.find(class_="col-xs-12 date-time p-x-0")
         visits_count = topic_data.find(class_="pull-xs-right").text.strip().replace(" بازدید", '')
@@ -61,27 +61,31 @@ def QA_datas(QA_URL_list):
 
         data = []
         for user in article:
-        # find all columns in each row
+            # find all columns in each row
             try:
                 nickname = user.find(class_="col-xs-9 col-md-12 text-md-center text-xs-right nickname").text.strip()
             except:
                 continue
             try:
-                posts_count_user = user.find(class_="text-xs-right pull-sm-right pull-md-none text-md-center post-count").text.strip().replace("تعداد پست: ", '')
+                posts_count_user = user.find(
+                    class_="text-xs-right pull-sm-right pull-md-none text-md-center post-count").text.strip().replace(
+                    "تعداد پست: ", '')
             except:
                 posts_count_user = None
             try:
-                message = user.find(class_="post-message topic-post__message col-xs-12 fr-view m-b-1 p-x-1").text.strip()
+                message = user.find(
+                    class_="post-message topic-post__message col-xs-12 fr-view m-b-1 p-x-1").text.strip()
             except:
                 message = None
             data.append([topic, visits_count, posts_count, nickname, posts_count_user, message])
 
         return data
 
+
 def QA_URLs(URL):
     page_num = 1
     QA_URL_list = list()
-    while page_num <= 2:
+    while page_num <= 5320:
         URL = URL + str(page_num)
         soup = scrape(URL)
         for link in soup.findAll('a', href=re.compile("/discussion/topic/")):
